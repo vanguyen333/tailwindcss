635482cd9fe2d26d66416893037ea0d88a490cdb
vanguyen333 <vanguyen333@yahoo.com>
2023-10-16 15:52:18 -0400

    added all necessary components and basic styling in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-function App() {
+const App = () => {
   return (
-    <>
-      {" "}
-      <h1 className="text-3xl text-blue-600 font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    <main className="relative">
+      Nav
+      <section className="xl:padding-1 wide:padding-r padding-b"></section>
+      <section className="padding">PopularProducts</section>
+      <section className="padding">SuperQuality</section>
+      <section className="padding padding-x py-10">Services</section>
+      <section className="padding">SpeicalOffers</section>
+      <section className="padding bg-pale-blue">CutomerReviews</section>
+      <section className="padding padding-x sm:py-32 py-16 w-full">
+        Subscribe
+      </section>
+      <section className="padding bg-black padding-x padding-t pb-8">
+        Footer
+      </section>
+    </main>
   );
-}
-
+};
 export default App;
+
+// function App() {
+//   return (
+//     <>
+//       {" "}
+//       <h1 className="text-3xl text-blue-600 font-bold underline">
+//         Hello world!
+//       </h1>
+//     </>
+//   );
+// }
+
+// export default App;
