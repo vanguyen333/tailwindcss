70ed68d6f721c9683e3444a0f31943395630b11b
vanguyen333 <vanguyen333@yahoo.com>
2023-10-16 19:29:47 -0400

    hero without styling left side

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,36 @@
+import { arrowRight } from "../assets/icons";
+import Button from "../components/Button";
+import { statistics } from "../constants";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section
+      id="home"
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
+    >
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+        <p>Our Summer Collection</p>
+        <h1>
+          <span>The New Arrival</span>
+          <br />
+          <span>Nike Shoes</span>
+        </h1>
+        <p>
+          Discover Stylish Nike arrivals, quality comfort, and innovation for
+          your active life.
+        </p>
+        <Button label="Shop now" iconURL={arrowRight} />
+        <div className="flex justify-starts items-start flex-wrap w-full mt-20 gap-16">
+          {statistics.map((stat) => (
+            <div key={stat.label}>
+              <p>{stat.value}</p>
+              <p>{stat.label}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
