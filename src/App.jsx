836f8f20b5d3881aaa5dda2bc64c66e557730dd5
vanguyen333 <vanguyen333@yahoo.com>
2023-10-16 16:37:17 -0400

    navbar items displayed

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,11 @@ import {
   Subscribe,
   Footer,
 } from "./sections";
+import Nav from "./components/Nav";
 const App = () => {
   return (
     <main className="relative">
-      Nav
+      <Nav />
       <section className="xl:padding-1 wide:padding-r padding-b">
         <Hero />
       </section>
---
+++ src/components/Nav.jsx
@@ -0,0 +1,29 @@
+// import headerLogo from "../assets/images";
+import headerLogo from "../assets/images/header-logo.svg";
+import hamburger from "../assets/icons/hamburger.svg";
+import { navLinks } from "../constants";
+const Nav = () => {
+  return (
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img src={headerLogo} alt="logo" width={130} height={29} />
+        </a>
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lag:hidden">
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className="font-monts errat leading-normal text-lg text-slate-gray"
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
