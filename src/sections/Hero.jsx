bc58083bd1745fb85cbe79330fbe3814d2197ca2
vanguyen333 <vanguyen333@yahoo.com>
2023-10-16 19:39:25 -0400

    done stylding left hero

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -9,13 +9,17 @@ const Hero = () => {
       className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
     >
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
-        <p>Our Summer Collection</p>
-        <h1>
-          <span>The New Arrival</span>
+        <p className="text-xl font-montserrat text-coral-red">
+          Our Summer Collection
+        </p>
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[720px] max-sm:leading-[82]">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
+            The New Arrival
+          </span>
           <br />
-          <span>Nike Shoes</span>
+          <span className="text-coral-red inline-blockmt-3">Nike </span> Shoes
         </h1>
-        <p>
+        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
           Discover Stylish Nike arrivals, quality comfort, and innovation for
           your active life.
         </p>
@@ -23,8 +27,10 @@ const Hero = () => {
         <div className="flex justify-starts items-start flex-wrap w-full mt-20 gap-16">
           {statistics.map((stat) => (
             <div key={stat.label}>
-              <p>{stat.value}</p>
-              <p>{stat.label}</p>
+              <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
+              <p className="leading-7 font-montserrat text-slate-gray">
+                {stat.label}
+              </p>
             </div>
           ))}
         </div>
