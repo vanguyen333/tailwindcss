085ee3658b39d341d8453886a60fd500d4294f15
vanguyen333 <vanguyen333@yahoo.com>
2023-10-16 16:07:28 -0400

    Created components template in sections folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,40 @@
+import {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  SpecialOffer,
+  CustomerReviews,
+  Subscribe,
+  Footer,
+} from "./sections";
 const App = () => {
   return (
     <main className="relative">
       Nav
-      <section className="xl:padding-1 wide:padding-r padding-b"></section>
-      <section className="padding">PopularProducts</section>
-      <section className="padding">SuperQuality</section>
-      <section className="padding padding-x py-10">Services</section>
-      <section className="padding">SpeicalOffers</section>
-      <section className="padding bg-pale-blue">CutomerReviews</section>
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding padding-x py-10">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffer />
+      </section>
+      <section className="padding bg-pale-blue">
+        <CustomerReviews />
+      </section>
       <section className="padding padding-x sm:py-32 py-16 w-full">
-        Subscribe
+        <Subscribe />
       </section>
       <section className="padding bg-black padding-x padding-t pb-8">
-        Footer
+        <Footer />
       </section>
     </main>
   );
