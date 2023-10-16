3ac7171f0035ed98eed39a3fd487080bfc763920
vanguyen333 <vanguyen333@yahoo.com>
2023-10-16 15:35:20 -0400

    starter project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      {" "}
+      <h1 className="text-3xl text-blue-600 font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
