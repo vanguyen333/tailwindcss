8bf9b9dd218d222c0a5bc4464de2df573897a1e4
vanguyen333 <vanguyen333@yahoo.com>
2023-10-17 23:01:54 -0400

    Done right side of SuperQuality section

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,3 +1,4 @@
+import { shoe8 } from "../assets/images";
 import Button from "../components/Button";
 const SuperQuality = () => {
   return (
@@ -22,6 +23,15 @@ const SuperQuality = () => {
         <div className="mt-11">
           <Button label="View details" />
         </div>
+      </div>
+      <div className="flex-1 flex justify-center items-center">
+        <img
+          src={shoe8}
+          alt="shoe8"
+          width={570}
+          height={522}
+          className="object-contain"
+        />
       </div>
     </section>
   );
