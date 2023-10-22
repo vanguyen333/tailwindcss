1b9c12a358c27996819d0f2801692f0abda7f2b5
vanguyen333 <vanguyen333@yahoo.com>
2023-10-22 17:34:46 -0400

    shoe card plain template imported successfully

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -14,7 +14,7 @@ const Nav = () => {
             <li key={item.label}>
               <a
                 href={item.href}
-                className="font-monts errat leading-normal text-lg text-slate-gray"
+                className="font-montserrat leading-normal text-lg text-slate-gray"
               >
                 {item.label}
               </a>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -39,7 +39,7 @@ const Hero = () => {
           ))}
         </div>
       </div>
-
+      {/* right hero side */}
       <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
         <img
           src={bigShoeImg}
